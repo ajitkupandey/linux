f017e99f4a4fd510fc3c61a78a4b765a3fb42b0c
Ajit Kumar Pandey <AjitKumar.Pandey@amd.com>
2021-07-12 23:58:28 +0530

    ASoC: SOF: amd: Add helper callbacks for ACP's DMA configuration
    ACP DMA is used for loading SOF firmware into DSP memory and data
    transfer from system memory to DSP memory. Add helper callbacks to
    initialize and configure ACP DMA block for fw loading.
    
    Signed-off-by: Ajit Kumar Pandey <AjitKumar.Pandey@amd.com>

--- sound/soc/sof/amd/acp.c
+++ sound/soc/sof/amd/acp.c
@@ -19,6 +19,568 @@
 #include "acp.h"
 #include "../ops.h"
 
+static void acp_enable_extirq(struct acp_dev_data *adata)
+{
+	int val = 0;
+	struct snd_sof_dev *sdev = adata->dev;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_EXTERNAL_INTR_ENB, 0x01);
+	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_EXTERNAL_INTR_CNTL);
+	val |= ACP_ERROR_MASK;
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_EXTERNAL_INTR_CNTL, val);
+	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_DSP_SW_INTR_CNTL);
+	val |= 0x01;
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_DSP_SW_INTR_CNTL, val);
+	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_DSP_SW_INTR_CNTL);
+
+}
+
+/* Group1 & Group2 with 4kb as page size mapped to ACP Scratch memory */
+static void configure_acp_groupregisters(struct acp_dev_data *adata)
+{
+	u32 index, val;
+	struct snd_sof_dev *sdev = adata->dev;
+
+	/* Group Enable */
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+		ACPAXI2AXI_ATU_BASE_ADDR_GRP_1,
+				ACP_SRAM_PTE_OFFSET | BIT(31));
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+		ACPAXI2AXI_ATU_PAGE_SIZE_GRP_1, PAGE_SIZE_4K_ENABLE);
+
+	for (index = 0; index < 2; index++) {
+		val = (ACP_SRBM_DATA_RAM_BASE_OFFSET + (index * 0x4000))
+		       | BIT(31);
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+			  ACPAXI2AXI_ATU_BASE_ADDR_GRP_2 + (index * 0x08),
+			  val);
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+				  ACPAXI2AXI_ATU_PAGE_SIZE_GRP_2 + (index * 0x08),
+				  PAGE_SIZE_4K_ENABLE);
+	}
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACPAXI2AXI_ATU_CTRL, 0x01);
+}
+
+static void configure_dma_descriptiors(struct acp_dev_data *adata)
+{
+	unsigned int dma_desc_base_addr;
+	struct snd_sof_dev *sdev = adata->dev;
+
+	dma_desc_base_addr = ACP_SRAM_PTE_OFFSET +
+			     offsetof(struct acp_scratch_register_config,
+				      acp_config_dma_desc);
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_DMA_DESC_BASE_ADDR,
+			  dma_desc_base_addr);
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_DMA_DESC_MAX_NUM_DSCR, 0x02);
+}
+
+static void acpbus_configure_dma_descriptor(struct acp_dev_data *adata,
+					    unsigned short dscr_idx,
+					    struct acp_config_dma_descriptor
+					    *dscr_info)
+{
+	unsigned int scratch_mem_offset =
+				(ACP_SCRATCH_REG_0 +
+				 offsetof(struct acp_scratch_register_config,
+					  acp_config_dma_desc)) +
+				(dscr_idx *
+				 sizeof(struct acp_config_dma_descriptor));
+
+	struct snd_sof_dev *sdev = adata->dev;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, scratch_mem_offset,
+			  dscr_info->src_addr);
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (scratch_mem_offset + 0x4),
+			  dscr_info->dest_addr);
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (scratch_mem_offset + 0x8),
+			  dscr_info->dma_transfer_count.u32_all);
+}
+
+static int acpbus_config_dma_channel(struct acp_dev_data *adata,
+				     unsigned char ch_num,
+				     unsigned short dscr_start_idx,
+				     unsigned short dscr_count,
+				     unsigned int priority_level)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+	int status = 0;
+	u32 val, acp_error_stat, dma_error_stat, acp_dma_dscr_cnt0;
+	u32 acp_dma_dscr_start_idx0, acp_dma_prio0, acp_dma_cntl0;
+	u32 timeout  = 0x00;
+
+	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+			      (ACP_DMA_CNTL_0 + (ch_num * sizeof(u32))));
+	dev_info(sdev->dev, "mACP_DMA_CNTL_0 : 0x%x\n", val);
+	val |= 0x11;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+			  (ACP_DMA_CNTL_0 + (ch_num * sizeof(u32))), 0x11);
+	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+			       ACP_DMA_CNTL_0 + (ch_num * sizeof(u32)));
+
+	dev_info(sdev->dev, "mACP_DMA_CNTL_0 : 0x%x\n", val);
+
+	while (1) {
+		val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_DMA_CH_RST_STS);
+		dev_info(sdev->dev, "ACP_DMA_CH_RST_STS : 0x%x\n", val);
+		val &= 0xFF;
+		if (!(val & (1 << ch_num))) {
+			udelay(5);
+			if (timeout > 0xff) {
+				dev_err(sdev->dev, "DMA_CHANNEL RESET ERROR TIMEOUT\n");
+				acp_error_stat = snd_sof_dsp_read(sdev,
+							ACP_DSP_BAR,
+							ACP_ERROR_STATUS);
+				dev_err(sdev->dev, "ACP_ERROR_STATUS :0x%x\n",
+				       acp_error_stat);
+				dma_error_stat = snd_sof_dsp_read(sdev,
+						ACP_DSP_BAR,
+						(ACP_DMA_ERR_STS_0 +
+						(ch_num * sizeof(u32))));
+				dev_err(sdev->dev, "ACP_DMA_ERR_STS :0x%x\n",
+				       dma_error_stat);
+				status = -ETIMEDOUT;
+				break;
+			}
+			timeout++;
+		} else {
+			dev_info(sdev->dev, "ACP_DMA_CH_RST_%d is done\n",
+				 ch_num);
+			break;
+		}
+	}
+
+	if (status < 0)
+		return status;
+	acp_dma_cntl0 = 0x00;
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (ACP_DMA_CNTL_0 +
+			  ch_num * sizeof(u32)), acp_dma_cntl0);
+
+	acp_dma_cntl0 =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+					  (ACP_DMA_CNTL_0 +
+					  (ch_num * sizeof(u32))));
+
+	dev_info(sdev->dev, "ACP_DMA_CNTL_0_%d : 0x%x\n", ch_num,
+		 acp_dma_cntl0);
+
+	acp_dma_dscr_cnt0 = dscr_count;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (ACP_DMA_DSCR_CNT_0 +
+			  (ch_num * sizeof(u32))), acp_dma_dscr_cnt0);
+
+	acp_dma_dscr_cnt0 =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+					      (ACP_DMA_DSCR_CNT_0 +
+					      (ch_num * sizeof(u32))));
+
+	dev_info(sdev->dev, "ACP_DMA_DSCR_CNT_0 : 0x%x\n",
+		 acp_dma_dscr_cnt0);
+
+	acp_dma_dscr_start_idx0 = dscr_start_idx;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (ACP_DMA_DSCR_STRT_IDX_0 +
+			  (ch_num * sizeof(u32))), acp_dma_dscr_start_idx0);
+
+
+	acp_dma_dscr_start_idx0 =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+						(ACP_DMA_DSCR_STRT_IDX_0 +
+						(ch_num * sizeof(u32))));
+	dev_info(sdev->dev, "ACP_DMA_DSCR_STRT_IDX_0 : 0x%x\n",
+		 acp_dma_dscr_start_idx0);
+	acp_dma_prio0 = priority_level;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (ACP_DMA_PRIO_0 +
+			  (ch_num * sizeof(u32))), acp_dma_prio0);
+	acp_dma_prio0 =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+				(ACP_DMA_PRIO_0 + (ch_num * sizeof(u32))));
+
+	dev_info(sdev->dev, "ACP_DMA_PRIO_0 : 0x%x\n", acp_dma_prio0);
+
+	acp_dma_cntl0 |= ACP_DMA_CH_RUN | ACP_DMA_CH_IOC_ENABLE;
+
+	snd_sof_dsp_write(sdev, ACP_DSP_BAR, (ACP_DMA_CNTL_0 +
+			  (ch_num * sizeof(u32))), acp_dma_cntl0);
+	acp_dma_cntl0 =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+				 (ACP_DMA_CNTL_0 + (ch_num * sizeof(u32))));
+	dev_info(sdev->dev, "ACP_DMA_CNTL_0_%d : 0x%x\n",
+		 ch_num, acp_dma_cntl0);
+	return  status;
+}
+
+static int acpbus_dma_start(struct acp_dev_data *adata,
+			    unsigned char *p_ch_num,
+			    unsigned short *p_dscr_count,
+			    struct acp_config_dma_descriptor *p_dscr_info,
+			    unsigned int priority_level)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+	int status = -1;
+	u16 dscr_start_idx = 0x00;
+	u16 dscr;
+	u16 dscr_count;
+	u16 dscr_num;
+
+	if ((p_ch_num) && (p_dscr_count) && (p_dscr_info)) {
+		status = 0x00;
+		dscr_count = *p_dscr_count;
+		/* currently channel number hard coded to zero,
+		 * As single channel is used for ACP DMA transfers
+		 * from host driver.
+		 * Below are the instances ACP DMA transfer
+		 * initiated from ACP Loader driver.
+		 */
+		*p_ch_num = 0x00;
+		for (dscr = 0; dscr < *p_dscr_count; dscr++) {
+			dscr_num = dscr_start_idx + dscr;
+			/* configure particular descriptor registers */
+			acpbus_configure_dma_descriptor(adata,
+							dscr_num,
+							p_dscr_info++);
+		}
+		/* configure the dma channel and start the dma transfer */
+		status = acpbus_config_dma_channel(adata,
+						   *p_ch_num,
+						   dscr_start_idx,
+						   *p_dscr_count,
+						    priority_level);
+		if (status < 0)
+			dev_err(sdev->dev,
+				"acpbus config dma channel failed:%d\n", status);
+	}
+	return status;
+}
+
+int acpbus_configure_and_run_dma(struct acp_dev_data *adata,
+					unsigned int src_addr,
+					unsigned int  dest_addr,
+					unsigned int dsp_data_size,
+					unsigned char *p_ch_num)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+	int status = 0x00;
+	u32 priority_level = 0x00;
+	unsigned int desc_count = 0;
+	unsigned char ch_no = 0xFF;
+	unsigned int index;
+
+
+	while (dsp_data_size >= ACP_PAGE_SIZE) {
+		adata->dscr_info[desc_count].src_addr = src_addr +
+							(desc_count *
+							 ACP_PAGE_SIZE);
+		adata->dscr_info[desc_count].dest_addr = dest_addr +
+							 (desc_count *
+							  ACP_PAGE_SIZE);
+		adata->dscr_info[desc_count].dma_transfer_count.bits.count =
+								ACP_PAGE_SIZE;
+		desc_count++;
+		dsp_data_size -= ACP_PAGE_SIZE;
+	}
+	if (dsp_data_size > 0) {
+		adata->dscr_info[desc_count].src_addr = src_addr +
+							(desc_count *
+							 ACP_PAGE_SIZE);
+		adata->dscr_info[desc_count].dest_addr = dest_addr +
+							 (desc_count *
+							  ACP_PAGE_SIZE);
+		adata->dscr_info[desc_count].dma_transfer_count.bits.count =
+								dsp_data_size;
+		adata->dscr_info[desc_count].dma_transfer_count.bits.ioc = 0x01;
+		desc_count++;
+	} else if (desc_count > 0) {
+		adata->dscr_info[desc_count].dma_transfer_count.bits.ioc = 0x01;
+	}
+	status = acpbus_dma_start(adata, &ch_no, (unsigned short *)&desc_count,
+				  adata->dscr_info, priority_level);
+	if (status == 0x00)
+		*p_ch_num = ch_no;
+	else
+		dev_err(sdev->dev, "acpbus_dma_start failed\n");
+	for (index = 0; index < desc_count; index++) {
+		memset(&adata->dscr_info[index], 0x00,
+		       sizeof(struct acp_config_dma_descriptor));
+	}
+	return status;
+}
+
+int acpbus_configure_and_run_sha_dma(struct acp_dev_data *adata,
+					     void *image_addr,
+					     unsigned int sha_dma_start_addr,
+					     unsigned int sha_dma_dest_addr,
+					     unsigned int image_length)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+	int  ret = 0;
+	int  val = 0;
+	int  sha_dma_status = 0;
+	u32  delay_count = 0;
+	u32  image_offset = 0;
+	u32  sha_message_length = 0;
+
+	u32      acp_sha_dma_start_addr;
+	u32      acp_sha_dma_dest_addr;
+	u32      acp_sha_msg_length;
+	u32      acp_sha_dma_cmd =  0;
+	u32      acp_sha_transfer_byte_count =  0;
+	u32      acp_sha_dsp_fw_qualifier = 0;
+	unsigned long jiffies = msecs_to_jiffies(3);
+	unsigned int value = 0;
+
+	if (image_addr) {
+		image_offset = 0;
+		sha_message_length = image_length;
+
+		val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_SHA_DMA_CMD);
+		if (val & 0x01) {
+			val = 0x02;
+			snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_SHA_DMA_CMD, val);
+			delay_count = 0;
+
+			while (1) {
+				val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_SHA_DMA_CMD_STS);
+				if (!(val & 0x02)) {
+					if (delay_count > 0x100) {
+						dev_err(sdev->dev, "SHA DMA Failed to Reset\n");
+						ret  = -1;
+						break;
+					}
+					udelay(1);
+					delay_count++;
+				} else {
+					val = 0x0;
+					snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+							  ACP_SHA_DMA_CMD, val);
+					break;
+				}
+			}
+		}
+		acp_sha_dma_start_addr = sha_dma_start_addr + image_offset;
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_SHA_DMA_STRT_ADDR,
+				  acp_sha_dma_start_addr);
+		acp_sha_dma_dest_addr = sha_dma_dest_addr;
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR,
+				  ACP_SHA_DMA_DESTINATION_ADDR,
+				  acp_sha_dma_dest_addr);
+		acp_sha_msg_length = sha_message_length;
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_SHA_MSG_LENGTH,
+				  acp_sha_msg_length);
+		/* enables SHA DMA IOC bit & Run Bit */
+		acp_sha_dma_cmd = 0x05;
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_SHA_DMA_CMD,
+				  acp_sha_dma_cmd);
+		acp_sha_dma_cmd = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+						   ACP_SHA_DMA_CMD);
+
+		delay_count = 0;
+		while (1) {
+			acp_sha_transfer_byte_count = snd_sof_dsp_read(sdev,
+							ACP_DSP_BAR,
+							ACP_SHA_TRANSFER_BYTE_CNT);
+
+			if (acp_sha_transfer_byte_count != sha_message_length) {
+				if (delay_count > 0xA00) {
+					dev_err(sdev->dev, "sha dma count :0X%x\n",
+					       acp_sha_transfer_byte_count);
+					ret  = -1;
+					break;
+				}
+				udelay(1);
+				delay_count++;
+			} else {
+				pr_info("sha dma transfer count :0X%x\n",
+					 acp_sha_transfer_byte_count);
+				break;
+			}
+		}
+
+		if (ret) {
+			dev_err(sdev->dev, "SHA DMA Failed to Transfer Message Length\n");
+			goto sha_exit;
+		}
+		sha_dma_status =
+		wait_event_interruptible_timeout(adata->wait_queue_sha_dma,
+						 adata->sha_irq_stat != 0,
+						 jiffies);
+		if (!sha_dma_status)
+			dev_info(sdev->dev,
+				 "sha dma waitqueue timeout error :0x%x\n",
+				 sha_dma_status);
+		else if (sha_dma_status < 0)
+			dev_err(sdev->dev, "sha dma wait queue error: 0x%x\n",
+				 sha_dma_status);
+		pr_info("sha irq status : 0x%x\n", adata->sha_irq_stat);
+		adata->sha_irq_stat  = 0;
+		value = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+					 ACP_SHA_TRANSFER_BYTE_CNT);
+		pr_info("ACP_SHA_TRANSFER_BYTE_CNT 0x%x\n", value);
+		value = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+					 ACP_SHA_DMA_ERR_STATUS);
+		pr_info("ACP_SHA_DMA_ERR_STATUS 0x%x\n", value);
+
+
+		pr_info("ACP_SHA_DSP_FW_QUALIFIER 0x%x\n",
+			 acp_sha_dsp_fw_qualifier);
+
+		acp_sha_dsp_fw_qualifier = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+						ACP_SHA_DSP_FW_QUALIFIER);
+
+		pr_info("ACP_SHA_DSP_FW_QUALIFIER 0x%x\n",
+			 acp_sha_dsp_fw_qualifier);
+		snd_sof_dsp_write(sdev, ACP_DSP_BAR, ACP_SHA_DSP_FW_QUALIFIER, 0x01);
+
+		acp_sha_dsp_fw_qualifier = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+						ACP_SHA_DSP_FW_QUALIFIER);
+		pr_info("ACP_SHA_DSP_FW_QUALIFIER 0x%x\n",
+			 acp_sha_dsp_fw_qualifier);
+
+		if ((acp_sha_dsp_fw_qualifier & 0x01) != 0x01)
+			dev_err(sdev->dev, "PSP validation failed on ACP FW/Plugin image\n");
+	}
+
+sha_exit:
+	return ret;
+}
+
+int acp_dma_status(struct acp_dev_data *adata, unsigned char ch_num)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+	int  status = 0x00;
+	u32  timeout = 0;
+	u32 acp_dma_cntl0 = 0, acp_dma_ch_stat = 0;
+	u32 val = 0;
+
+	acp_dma_cntl0 = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+					 ACP_DMA_CNTL_0 +
+					 (ch_num * sizeof(u32)));
+	if (acp_dma_cntl0 & 0x02) {
+		dev_info(sdev->dev, "DMA_CHANNEL_%d run bit set\n", ch_num);
+		while (1) {
+			acp_dma_ch_stat = snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+							   ACP_DMA_CH_STS);
+			if (acp_dma_ch_stat & (1 << ch_num)) {
+				udelay(1);
+				if (timeout > 3000) {
+					dev_err(sdev->dev, "DMA_CHANNEL status error timeout\n");
+					status = -ETIMEDOUT;
+					break;
+				}
+				timeout++;
+			} else {
+				dev_info(sdev->dev,
+					 "DMA_CHANNEL_%d transfer completed\n",
+					 ch_num);
+				break;
+			}
+		}
+	} else {
+		dev_err(sdev->dev, "DMA_CHANNEL_%d run bit not set\n", ch_num);
+	}
+	val =  snd_sof_dsp_read(sdev, ACP_DSP_BAR,
+			(ACP_DMA_ERR_STS_0 + (ch_num * sizeof(u32))));
+	dev_info(sdev->dev, "ACP_DMA_ERR_STS_0 : 0x%x\n", val);
+	return status;
+}
+
+static void acpbus_init_scratch_memory(struct acp_dev_data *adata,
+					struct AcpScratchRegisterConfig *scratch_buf_info)
+{
+	writel(0, (u32 *)(&scratch_buf_info->sofHostMsgWrite));
+	writel(0, (u32 *)(&scratch_buf_info->sofHostAckwrite));
+}
+
+static void  acpbus_config_command_resp_buffers(struct acp_dev_data *adata)
+{
+	acpbus_init_scratch_memory(adata,
+					&adata->acp_sregmap->scratch_buf_info);
+}
+
+void acp_copy_from_scratch_memory(struct snd_sof_dev *sdev,
+				  unsigned char *ring_buffer, u32  offset,
+				  void *read_buffer, u32 bytes_to_write)
+{
+		int index;
+		int val = 0;
+		unsigned int *src_buf = (unsigned int *)(ring_buffer + offset);
+		unsigned int *dst_buf = (unsigned int *)read_buffer;
+		unsigned int scratch_reg = 0;
+
+		for (index = 0; index < (bytes_to_write / 4); index++) {
+			val = readl((u32 *)(src_buf + index));
+			scratch_reg = (ACP_SCRATCH_REG_0 + (index * 4));
+			*(dst_buf + index) = val;
+		}
+}
+
+void acp_copy_to_scratch_memory(struct snd_sof_dev *sdev,
+				unsigned char *ring_buffer,
+				u32  offset,
+				void *write_buffer,
+				u32 bytes_to_write)
+{
+	int index;
+	unsigned int *src_buf = (unsigned int *)write_buffer;
+	unsigned int *dest_buf = (unsigned int *)ring_buffer;
+
+	for (index = 0; index < (bytes_to_write / 4); index++)
+		writel(*(src_buf + index),
+				(u32 *)(dest_buf + (offset / 4) + index));
+}
+
+static void init_scratch_memory(struct acp_dev_data *adata)
+{
+	struct snd_sof_dev *sdev = adata->dev;
+
+	adata->acp_sregmap = (struct acp_scratch_register_config *)
+			     (sdev->bar[ACP_DSP_BAR] + ACP_SCRATCH_REG_0 -
+			      ACPBUS_REG_BASE_OFFSET);
+
+	if (adata->acp_sregmap == NULL)
+		dev_err(sdev->dev, "failed to assign acp regmap\n");
+
+
+	memset(adata->acp_sregmap_context, 0x00,
+	       sizeof(struct acp_scratch_register_config));
+	acp_copy_to_scratch_memory(sdev, (unsigned char *)adata->acp_sregmap,
+				   0x00,
+				   adata->acp_sregmap_context,
+				   sizeof(struct acp_scratch_register_config));
+	acpbus_config_command_resp_buffers(adata);
+}
+
+static int acp_probe_continue(struct acp_dev_data *adata)
+{
+	struct pci_dev *pci;
+	struct snd_sof_dev *sdev = adata->dev;
+
+	pci = pci_get_device(PCI_VENDOR_ID_AMD, 0x15e2, NULL);
+	acp_enable_extirq(adata);
+	configure_acp_groupregisters(adata);
+	configure_dma_descriptiors(adata);
+	adata->acp_sregmap_context =
+		devm_kzalloc(sdev->dev,
+			     sizeof(struct acp_scratch_register_config),
+			     GFP_KERNEL);
+	if (!adata->acp_sregmap_context) {
+		dev_err(sdev->dev, "buffer allocation failed for sregmap_conext\n");
+		return -ENOMEM;
+	}
+	init_scratch_memory(adata);
+	return 0;
+}
+
+static void acp_probe_work(struct work_struct *work)
+{
+	int ret = 0;
+	struct acp_dev_data *adata = container_of(work, struct acp_dev_data,
+						    probe_work);
+	struct snd_sof_dev *sdev = adata->dev;
+
+	ret = acp_probe_continue(adata);
+	if (ret < 0)
+		dev_err(sdev->dev, "acp fw load sequence failed\n");
+}
 int acp_power_on(struct snd_sof_dev *sdev)
 {
 	u32 val;
@@ -74,7 +636,6 @@ static irqreturn_t acp_irq_thread(int irq, void *context)
 	u16 acp_flag = 0;
 	unsigned int val = 0;
 	struct snd_sof_dev *sdev = context;
-	struct acp_dev_data *adata = sdev->pdata->hw_pdata;
 
 	val = snd_sof_dsp_read(sdev, ACP_DSP_BAR, ACP_EXTERNAL_INTR_STAT);
 
@@ -155,6 +716,11 @@ int amd_sof_acp_probe(struct snd_sof_dev *sdev)
 		dev_err(sdev->dev, "error: failed ACP init\n");
 		goto out;
 	}
+
+	INIT_WORK(&adata->probe_work, acp_probe_work);
+	init_waitqueue_head(&adata->wait_queue_sha_dma);
+
+	schedule_work(&adata->probe_work);
 
 	return ret;
 out:
--- sound/soc/sof/amd/acp.h
+++ sound/soc/sof/amd/acp.h
@@ -25,11 +25,155 @@
 #define ACP_SOFT_RESET_DONE_MASK		0x00010001
 #define ACP_EXT_INTR_ERROR_STAT			0x20000000
 
+#define ACP_SRAM_PTE_OFFSET			0x02050000
+#define ACP_ERROR_MASK				0x20000000
+#define ACP_SHA_STAT				0x8000
+#define FW_BIN_PTE_OFFSET			0x00
+#define FW_DATA_BIN_PTE_OFFSET			0x08
+#define PAGE_SIZE_4K_ENABLE			0x2
+#define ACP_SRBM_DATA_RAM_BASE_OFFSET		0x0100C000
+#define ACP_PAGE_SIZE				0x1000
+#define ACP_DMA_CH_RUN				0x02
+#define ACP_DMA_CH_IOC_ENABLE			0x04
+#define ACPBUS_REG_BASE_OFFSET			ACP_DMA_CNTL_0
+
+/* DSP hardware descriptor */
+struct sof_amd_dsp_desc {
+	int cores_num;
+	int acp_reg_base;
+	int acp_reg_end;
+	int num_res;
+	char *dev_name;
+	struct resource *res;
+	int num;
+};
+
+struct  acp_atu_grp_pte {
+	u32 addr_low;
+	u32 addr_high;
+};
+
+union acp_config_dma_trnsfer_cnt {
+	struct {
+		unsigned int  count : 19;
+		unsigned int  reserved : 12;
+		unsigned  ioc : 1;
+	} bitfields, bits;
+	unsigned int    u32_all;
+	signed int  i32_all;
+};
+
+struct acp_config_dma_descriptor {
+	unsigned int    src_addr;
+	unsigned int    dest_addr;
+	union acp_config_dma_trnsfer_cnt  dma_transfer_count;
+	unsigned int	reserved;
+};
+
+/* Scratch memory structure for communication b/w
+ * host and dsp.
+ */
+
+struct  AcpScratchRegisterConfig {
+
+	/* DSP mailbox */
+	u8      sofOutBoxBuffer[512];
+	/* Host mailbox */
+	u8      sofInBoxBuffer[512];
+	/* Debug memory */
+	u8      sofDebugBuffer[1024];
+	/* Exception memory*/
+	u8      sofExceptBuffer[1024];
+	/* Stream buffer */
+	u8      sofStreamBuffer[1024];
+	/* Trace buffer */
+	u8      sofTraceBuffer[1024];
+	/* Host msg flag */
+	u32     sofHostMsgWrite;
+	/* Host ack flag*/
+	u32     sofHostAckwrite;
+	/* DSP msg flag */
+	u32     sofDspMsgWrite;
+	/* Dsp ack flag */
+	u32     sofDspAckwrite;
+};
+
+/* acp_scratch_register_config structure used to map scratch memory
+ * acp_atu_grp1_pte - Group1 PTE entries structure
+ * acp_config_dma_desc - acp dma descriptor structure
+ * acp_cmd_ring_buf_info - this structure hold information about
+ * acp command ring buffer like write_index, current_write_index
+ * acp_cmd_buf - acp command ring buffer
+ * acp_resp_ring_buf_info - acp response ring buffer information
+ * such as read index, write index
+ * acp_resp_buf - acp response ring buffer
+ */
+struct  acp_scratch_register_config {
+	struct AcpScratchRegisterConfig scratch_buf_info;
+	struct acp_atu_grp_pte    acp_atu_grp1_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp2_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp3_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp4_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp5_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp6_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp7_pte[16];
+	struct acp_atu_grp_pte    acp_atu_grp8_pte[16];
+	struct acp_config_dma_descriptor	acp_config_dma_desc[64];
+	unsigned int reg_offset[8];
+	unsigned int buf_size[8];
+	u8      acpTransmitFifoBuffer[256];
+	u8      acpReceiveFifoBuffer[256];
+	unsigned int	reserve[];
+};
+
+enum buffer_type {
+	FW_BIN = 0,
+	FW_DATA_BIN,
+};
 /* Common device data struct for ACP devices */
 struct acp_dev_data {
 	struct snd_sof_dev  *dev;
+	struct acp_scratch_register_config *acp_sregmap;
+	struct acp_scratch_register_config *acp_sregmap_context;
+	unsigned int fw_bin_size;
+	unsigned int fw_data_bin_size;
+
+	u32 fw_bin_page_count;
+	dma_addr_t sha_dma_addr;
+	u8 *bin_buf;
+	dma_addr_t dma_addr;
+	u8 *data_buf;
+
+	u32 sha_irq_stat;
+	struct work_struct probe_work;
+
+	wait_queue_head_t wait_queue_sha_dma;
+	struct acp_config_dma_descriptor dscr_info[128];
 };
 
+/* Generic ACP helper services */
+void acp_copy_to_scratch_memory(struct snd_sof_dev *sdev,
+				unsigned char *ring_buffer,
+				u32  offset,
+				void *write_buffer,
+				u32 bytes_to_write);
+void acp_copy_from_scratch_memory(struct snd_sof_dev *sdev,
+				  unsigned char *ring_buffer,
+				  u32  offset,
+				  void *read_buffer,
+				  u32 bytes_to_write);
+
+int acp_dma_status(struct acp_dev_data *adata, unsigned char ch_num);
+int acpbus_configure_and_run_dma(struct acp_dev_data *adata,
+				 unsigned int src_addr,
+				 unsigned int dest_addr,
+				 unsigned int dsp_data_size,
+				 unsigned char *p_ch_num);
+int  acpbus_configure_and_run_sha_dma(struct acp_dev_data *adata,
+				      void *image_addr,
+				      unsigned int sha_dma_start_addr,
+				      unsigned int sha_dma_dest_addr,
+				      unsigned int image_length);
 /* ACP device probe/remove */
 int amd_sof_acp_probe(struct snd_sof_dev *sdev);
 int amd_sof_acp_remove(struct snd_sof_dev *sdev);
